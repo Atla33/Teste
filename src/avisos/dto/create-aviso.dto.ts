516ab6612b165c23b5b248822c9118ef07e3b513
Atla33 <dantasanderson57@hotmail.com>
2023-06-22 15:17:06 -0300

    Feito algumas alterações em aviso e sugestões

--- src/avisos/dto/create-aviso.dto.ts
+++ src/avisos/dto/create-aviso.dto.ts
@@ -1,17 +1,18 @@
 import { Aviso } from "@prisma/client";
-import { IsDate, IsDateString, IsString } from "class-validator";
+import { IsDate, IsDateString, IsString, Length } from "class-validator";
 
 export class CreateAvisoDto implements Aviso {
 
     avisoId: number;
-    @IsString()
+    @IsString({ message: "O título deve ser uma string." })
+    @Length(5, 50, { message: "O título deve ter entre 5 e 50 caracteres." })
     titulo: string;
-    @IsDateString()
+    @IsDate({ message: "A data deve estar no formato ISO 8601." })
     data: Date;
-    @IsString()
-
+    @IsString({ message: "O autor deve ser uma string." })
     autor: string;
-    @IsString()
-
+    @IsString({ message: "O texto deve ser uma string." })
+    @Length(10, 500, { message: "O texto deve ter entre 10 e 500 caracteres." })
     texto: string;
 }
+
--- src/sugestoes/dto/create-sugestoe.dto.ts
+++ src/sugestoes/dto/create-sugestoe.dto.ts
@@ -1,17 +1,17 @@
 import { Sugestao } from "@prisma/client";
-import { IsDate, IsDateString, IsString } from "class-validator";
+import { IsDate, IsDateString, IsString, Length } from "class-validator";
 
-export class CreateSugestoeDto implements Sugestao{
+export class CreateSugestoeDto implements Sugestao {
 
     sugestoesId: number;
-    @IsString()
+    @IsString({ message: "O nome do aluno deve ser uma string." })
+    @Length(5, 50, { message: "O nome do aluno deve ter entre 5 e 50 caracteres." })
     nome_aluno: string;
-    @IsString()
+    @IsString({ message: "O email do aluno deve ser uma string." })
     email_aluno: string;
-    @IsString()
+    @IsString({ message: "O comentário do aluno deve ser uma string." })
+    @Length(10, 500, { message: "O comentário do aluno deve ter entre 10 e 500 caracteres." })
     comentario_aluno: string;
-    @IsDateString()
+    @IsDate({ message: "A data deve estar no formato ISO 8601." })
     data: Date;
-
-
 }
